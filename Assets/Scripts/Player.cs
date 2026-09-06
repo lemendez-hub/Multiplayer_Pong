b776a0476f62078d1eb420e2cc652cd65cf17623
lemendez-hub <lemendez@csumb.edu>
2026-09-06 10:05:01 -0700

    Player
    Completed the host/client session startup.
    There is one spawned paddle prefab for the host and client.
    Host and client move their own paddles.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,71 @@
+using Unity.Netcode;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : NetworkBehaviour
+{
+    private InputSystemActions input_system_actions;
+
+    [Header("Player Properties")]
+    [SerializeField] private float player_speed = 10f;
+
+    private Rigidbody player_rb;
+
+    private Vector2 player_input;
+
+    private void Awake()
+    {
+        input_system_actions = new InputSystemActions();
+
+        player_rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        if(!IsOwner)
+        {
+            return;
+        }
+        
+        MoveRPC(player_input);
+    }
+
+    [Rpc(SendTo.Server)]
+    private void MoveRPC(Vector2 input)
+    {
+        Vector3 movement = new Vector3(0f, input.y, 0f) * player_speed * Time.deltaTime;
+
+        player_rb.MovePosition(player_rb.position + movement);
+    }
+
+    public override void OnNetworkSpawn()
+    {
+        if(!IsOwner)
+        {
+            return;
+        }
+
+        input_system_actions.Enable();
+        
+        input_system_actions.Player.Move.performed += OnMove;
+        input_system_actions.Player.Move.canceled += OnMove;
+    }
+
+    public override void OnNetworkDespawn()
+    {
+        if(!IsOwner)
+        {
+            return;
+        }
+
+        input_system_actions.Player.Move.performed -= OnMove;
+        input_system_actions.Player.Move.canceled -= OnMove;
+
+        input_system_actions.Disable();
+    }
+
+    private void OnMove(InputAction.CallbackContext ctx)
+    {
+        player_input = ctx.ReadValue<Vector2>();
+    }
+}
